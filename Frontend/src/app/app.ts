90b0e1345e5882aade810d5be0b4b6f3e5f05cc8
Zsolt <5047426+bodzaital@users.noreply.github.com>
2025-11-16 15:24:36 +0100

    Add API consumer demo

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherForecast } from './weatherForecast';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Frontend');
+
+  protected weatherForecast: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {
+    http.get<WeatherForecast[]>("http://localhost:5276/weatherforecast/").subscribe({
+      next: (e) => this.weatherForecast = e,
+      error: (e) => console.error("Failed to query endpoint.", e)
+    });
+  }
 }
